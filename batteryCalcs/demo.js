53a9a65173dd41af3471bbbef350b5bc59610de3
Thibault Calliauw <calliauw.t@gmail.com>
2024-10-10 11:47:42 +1100

    comments in demo.js

--- batteryCalcs/demo.js
+++ batteryCalcs/demo.js
@@ -1,8 +1,7 @@
 import { generateAdvice } from './functions.js';
 
-// Example of monthly bill of 200, does not have solar and solar advise calculated by 28W platform is 10 kW system)
+// Example of monthly bill of 200, has solar of 10 kW system
 console.log(generateAdvice(200, true, 10));
 
-// Example of monthly bill of 200, has solar of 10 kW system
-
+// Example of monthly bill of 200, does not have solar and solar advise calculated by 28W platform is 6 kW system)
 console.log(generateAdvice(200, false, 6));
